c163436b382b833f81b9df4de1c099d548f66c65
Alexandre Araujo <alexandreh.araujo@gmail.com>
2016-11-28 01:40:23 -0200

    define CRUD methods

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -6,27 +6,94 @@
  */
 
 module.exports = {
+  /**
+   *
+   * @param req
+   * @param res
+   */
   list: function (req, res) {
-
-    return res.json([{name: 'Donald Trump'}]);
+    Users.find().exec({
+      error: function (err) {
+        return res.badRequest(err)
+      },
+      success: function(users) {
+        return res.json(users)
+      }
+    })
   },
+
+  /**
+   *
+   * @param req
+   * @param res
+   */
   store: function (req, res) {
-    return res.view();
+    Users.create(req.params.all()).exec({
+      error: function (err) {
+        return res.badRequest(err)
+      },
+      success: function (user) {
+        return res.json(user)
+      }
+    })
   },
+
+  /**
+   *
+   * @param req
+   * @param res
+   */
   show: function (req, res) {
-    var id = req.param('id');
+    var id = req.param('id')
 
-    return res.view();
+    Users.find({"id":id}).exec({
+      error: function (err) {
+        return res.badRequest(err)
+      },
+      success: function (users) {
+        if (users.length < 1) {
+          return res.notFound('user not found')
+        }
+
+        return res.json(users[0])
+      }
+    })
   },
+
+  /**
+   *
+   * @param req
+   * @param res
+   */
   update: function (req, res) {
-    var id = req.param('id');
+    var id = req.param('id')
 
-    return res.view();
+    Users.update({"id":id}, req.params.all()).exec({
+      error: function (err) {
+        return res.badRequest(err)
+      },
+      success: function (users) {
+        return res.json(users[0])
+      }
+    })
   },
+
+  /**
+   *
+   * @param req
+   * @param res
+   */
   destroy: function (req, res) {
-    var id = req.param('id');
+    var id = req.param('id')
 
-    return res.view();
-  },
+    Users.destroy({"id":id}).exec({
+      error: function (err) {
+        return res.badRequest(err)
+      },
+      success: function () {
+        return res.ok(204)
+      }
+    })
+  }
 };
 
