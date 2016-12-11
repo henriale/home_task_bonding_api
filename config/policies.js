33dcbe741af7944b84aeaaeec28333158969c286
Alexandre Araujo <alexandreh.araujo@gmail.com>
2016-12-11 16:37:07 -0200

    use auth policy for home, task and user controllers

--- config/policies.js
+++ config/policies.js
@@ -48,4 +48,16 @@ module.exports.policies = {
 		// before letting any users feed our rabbits
 		// feed : ['isNiceToAnimals', 'hasRabbitFood']
 	// }
+
+  UsersController: {
+    '*': 'sessionAuth'
+  },
+
+  HomesController: {
+    '*': 'sessionAuth'
+  },
+
+  TasksController: {
+    '*': 'sessionAuth'
+  }
 };
