ae2158a6d03e23675b17f36e01f2a8a60ff45eae
Alexandre Araujo <alexandreh.araujo@gmail.com>
2016-12-25 16:17:29 -0200

    setup test environment

---
+++ test/bootstrap.test.js
@@ -0,0 +1,26 @@
+let sails = require('sails');
+
+before(function(done) {
+
+  // Increase the Mocha timeout so that Sails has enough time to lift.
+  this.timeout(5000);
+
+  sails.lift({
+    port: 3333
+
+    // configuration for testing purposes
+  }, function(err, server) {
+    if (err) return done(err);
+
+    // here you can load fixtures, etc.
+
+    done(err, sails);
+  });
+});
+
+after(function(done) {
+
+  // here you can clear fixtures, etc.
+
+  sails.lower(done);
+});
