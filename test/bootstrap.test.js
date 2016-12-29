5a31e47634a07d8dee76590685a2277cf90b339e
Alexandre Araujo <alexandreh.araujo@gmail.com>
2016-12-29 08:37:57 -0200

    define bootstrap for testing

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -3,7 +3,7 @@ let sails = require('sails');
 before(function(done) {
 
   // Increase the Mocha timeout so that Sails has enough time to lift.
-  this.timeout(5000);
+  this.timeout(10000);
 
   sails.lift({
     port: 3333
